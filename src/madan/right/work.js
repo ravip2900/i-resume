acd440698ef723dbff5dd3d3eed8c0af3480dfd0
Ravi <ravip.iiit@gmail.com>
2022-12-26 17:47:22 +0530

    madan resume

---
+++ src/madan/right/work.js
@@ -0,0 +1,31 @@
+export default [{
+    time: 'March 2020 - Present',
+    company: 'Senior Software Engineer - Lowes India Pvt Ltd',
+    location: 'Bangalore, Karnataka',
+    technologies: 'React JS, Node JS, Hapi JS, Webpack, Mfe,  Redux, Jest, WebDriver IO, Javascript, Css, Html, Jenkins, Gcp, Kubernetes, Bitbucket, Git, Eslint.',
+    description: [
+        'Develop web user interfaces, Improve web accessibility and page performance, integrate various web services and write unit, automation testing.',
+    ]
+},{
+    time: 'January 2018 - February 2020',
+    company: 'Full Stack Developer - Sneed Technologies Pvt Ltd',
+    location: 'Bangalore, Karnataka',
+    technologies: 'React JS, React Native, Node JS, Express JS, Mongo DB, AWS,  Elastic Search, Backbone JS, Redux, Webpack, Grunt, Sass, Javascript, Html, Css, JQuery, Jade, Ejs.',
+    description: [
+        'Leader of the tech team, involved in each part of the work from design to develop and deploy.',
+        'Configured Elastic Search and Implement Search Apis.',
+        'Implemented SEO Techniques and written isomorphic javascript programming to improve website speed and performance.',
+        'Maintain and deploy AWS EC2 instances for production and test environments.',
+        'Integrated Payment gateways and Email, Whatsapp, Sms Communication Systems.',
+        'Implemented Role Based Access Control System.'
+    ]
+}, {
+    time: 'January 2016 - August 2017',
+    company: 'Associate Software Engineer - Tata Consultancy Services',
+    location: 'Chennai, Tamilnadu',
+    technologies: 'Java/J2EE, Mysql, javascript, Html, Css',
+    description: [
+        'Working for an Insurance Company.',
+        'Write, design or edit web pages and web apis.'
+    ]
+}];
