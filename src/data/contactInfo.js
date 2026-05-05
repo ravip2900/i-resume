27d28cc3255c8f887bcab330878b2e8e370c84eb
Ravi P <ravip.iiit@gmail.com>
2026-05-06 00:13:52 +0530

    email fix

--- src/data/contactInfo.js
+++ src/data/contactInfo.js
@@ -11,7 +11,7 @@ import {
 export const contactInfo = [
   { id: 'location', icon: LocationOn, text: 'Bengaluru, India' },
   { id: 'phone', icon: Phone, text: '+91 9493152900' },
-  { id: 'email', icon: Email, text: 'ravip.itt@gmail.com' },
+  { id: 'email', icon: Email, text: 'ravip.iiit@gmail.com' },
   { id: 'linkedin', icon: LinkedIn, text: 'linkedin.com/in/ravip2900' },
   { id: 'github', icon: GitHub, text: 'github.com/ravip2900' },
   {id: 'leetcode', icon: CodeRounded, text: 'leetcode.com/ravip2900'}
